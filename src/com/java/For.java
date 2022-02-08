79d02fd634bb3e7d0505686c9f648462a3731291
Guru424 <guru.c142@gmail.com>
2022-02-08 20:14:34 +0530

    first

---
+++ src/com/java/For.java
@@ -0,0 +1,14 @@
+package com.java;
+
+public class For {
+public static void main(String[] args) {
+	//Creating new for loop
+	for(int i=20;i>10;i--){
+		System.out.println("Value of i is ="+i);
+	}
+
+for(int i=20;i>10;i--){
+		System.out.println("Value of i is ="+i);
+	}
+}
+}
